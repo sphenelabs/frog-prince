d4fe57d2f6050109a130505fb1de5d4afdb1cf6a
ehsueh <emma.yc.hsueh@gmail.com>
2024-03-23 13:59:13 -0400

    wip

--- index.js
+++ index.js
@@ -16,18 +16,12 @@ const WALLET_ADDRESS = process.env.WALLET_ADDRESS;
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
 const RPC_URL = process.env.RPC_URL;
 
-const WALLET_ADDRESS_2 = process.env.WALLET_ADDRESS_2;
-
 const provider = new Provider(WALLET_PRIVATE_KEY, RPC_URL);
 const web3 = new Web3(provider);
 const contract = new web3.eth.Contract(SMART_CONTRACT_ABI, SMART_CONTRACT_ADDRESS);
 
 const promptContractABIPath = "./prompt.abi.json";
 const promptContractAddress = "0x64BF816c3b90861a489A8eDf3FEA277cE1Fa0E82"
-// const promptContract = new web3.eth.Contract(require(promptContractABIPath), promptContractAddress, { from: WALLET_ADDRESS });
-
-// example of calling a read method and get the return: await myContract.methods["balanceOf"](address).call();
-// example of calling a read method and get the return: await myContract.methods.balanceOf(address).call();
 
 app.get("/", async (req, res) => {
     try {
@@ -55,8 +49,11 @@ app.post('/register', async (req, res) => {
     try {
         const wallet = Wallet.createRandom();
         console.log("Created wallet", wallet);
-        const contractInteraction = createContractInteraction(wallet.privateKey);
-        await contractInteraction.register(wallet.address, descriptor); // Use the wallet address as the sender
+        const provider = new Provider(WALLET_PRIVATE_KEY, RPC_URL);
+        const web3 = new Web3(provider);
+        const contract = new web3.eth.Contract(SMART_CONTRACT_ABI, SMART_CONTRACT_ADDRESS);
+        const data = await contract.methods.register(username, descriptor).send({ from: wallet.address });
+        console.log(data)
         res.json({ address: wallet.address, privateKey: wallet.privateKey, mnemonic: wallet.mnemonic.phrase, username });
     } catch (error) {
         console.error(error);
@@ -65,8 +62,15 @@ app.post('/register', async (req, res) => {
 });
 
 
-// Use this method to send a message
+// Use this method to send a message via XMTP if the recipient is on the XMTP network
+// Otherwise, send with web2 but count the messaging on-chain
 // TODO should infer sender from header token, for now, just grab from request body
+// req.body = {
+//   fromAddress: string,
+//   toAddress: string,
+//   message: string
+// }
+// returns the messages in the conversation
 app.post("/croak", async (req, res) => {
 
     const { fromAddress, toAddress, message } = req.body;
@@ -81,35 +85,28 @@ app.post("/croak", async (req, res) => {
 
         const xmtp = await Client.create(signer, { env: "dev" });
         
-        // Start a conversation with XMTP
-        const conversation = await xmtp.conversations.newConversation(toAddress);
-        // Load all messages in the conversation
-        const messages = await conversation.messages();
-        // Send a message
-        await conversation.send(message);
-        // Listen for new messages in the conversation
-        for await (const message of await conversation.streamMessages()) {
-            console.log(`[${message.senderAddress}]: ${message.content}`);
+        const isOnXMTPNetwork = await xmtp.canMessage(toAddress);
+        if (isOnXMTPNetwork) {
+            // Start a conversation with XMTP
+            const conversation = await xmtp.conversations.newConversation(toAddress);
+            // Load all messages in the conversation
+            const messages = await conversation.messages();
+            // Send a message
+            await conversation.send(message);
+            // Listen for new messages in the conversation
+            for await (const message of await conversation.streamMessages()) {
+                console.log(`[${message.senderAddress}]: ${message.content}`);
+            }
         }
         // Call smart contract
-        // const data = await contract.methods.croak(toAddress).send({ from: fromAddress });
-        // console.log(data);
+        const data = await contract.methods.croak(toAddress).send({ from: fromAddress });
+        console.log(data);
         res.json({ value: messages });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, message: `Error croaking!` });
     }
 
-});
-
-app.get("/balanceOfWithAddress/", async (req, res) => {
-    try {
-        const data = await contract.methods.balanceOf(WALLET_ADDRESS);
-        res.json({ value: data });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: 'Error fetching data from the smart contract' });
-    }
 });
 
 app.get("/frogs", async (req, res) => {
@@ -119,6 +116,19 @@ app.get("/frogs", async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, message: 'Error fetching data from the smart contract' });
+    }
+});
+
+// Endpoint to hatch a tadpole (mint as NFT)
+app.post("/hatchTadpole", async (req, res) => {
+    const { fromAddress, toAddress } = req.body;
+    try {
+        var receipt = await contract.methods.hatchTadpole(toAddress).send({ from: fromAddress });
+        console.log("receipt", receipt);
+        res.json({ value: receipt });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Error minting tadpoles' });
     }
 });
 
